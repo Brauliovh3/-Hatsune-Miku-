03f63aa4d51d35c650e794f6ac25df83a3e78277
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-05 22:16:32 -0500

    Update rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -9,10 +9,10 @@ let usersLim = sortedLim.map(enumGetKey)
 let usersLevel = sortedLevel.map(enumGetKey)
 let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
 let text = `
-╭───═[ *Top ${len} Estrellas ⭐* ]═────⋆
+╭───═[ *Top ${len} Cebollines 🌱* ]═────⋆
 │╭───────────────···
 ✩│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩│ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} ⭐*`).join`\n✩│ `}
+✩│ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} 🌱*`).join`\n✩│ `}
 │╰────────────────···
 ╰───────────═┅═──────────
 
