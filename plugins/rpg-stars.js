ac25e8444949cb91632880a0f93022bd3f31de9d
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-05 22:21:53 -0500

    Update rpg-stars.js

--- plugins/rpg-stars.js
+++ plugins/rpg-stars.js
@@ -5,20 +5,20 @@ let handler = async (m, {conn, usedPrefix}) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let user = global.db.data.users[who]
     let name = conn.getName(who)
-    if (!(who in global.db.data.users)) return conn.reply(m.chat, '🚩 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
-    let img = await (await fetch(`https://tinyurl.com/yrkgv3qm`)).buffer()
-    let txt = ` –  *S T A R S  -  U S E R*\n\n`
-        txt += `┌  ✩  *Nombre* : ${user.name}\n`
-        txt += `│  ✩  *Estrellas* : ${toNum(user.limit)} ( *${user.limit}* )\n`
-        txt += `│  ✩  *Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
-        txt += `└  ✩  *XP* : ${toNum(user.exp)} ( *${user.exp}* )`
+    if (!(who in global.db.data.users)) return conn.reply(m.chat, '💙 El usuario no se encuentra en mi base de Datos.', m, rcanal).then(_ => m.react('✖️'))
+    let img = await (await fetch(`https://tinyurl.com/227nz7fs`)).buffer()
+    let txt = ` –  *C E B O L L I N E S  -  U S E R*\n\n`
+        txt += `┌  💙  *Nombre* : ${user.name}\n`
+        txt += `│  💙  *Cebollines* : ${toNum(user.limit)} ( *${user.limit}* )\n`
+        txt += `│  💙  *Bank* : ${toNum(user.bank)} ( *${user.bank}* )\n`
+        txt += `└  💙  *XP* : ${toNum(user.exp)} ( *${user.exp}* )`
     let mentionedJid = [who]
         
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
 }
-handler.help = ['estrellas']
+handler.help = ['Cebollines']
 handler.tags = ['rpg']
-handler.command = ['coins', 'wallet', 'cartera', 'estrellas', 'stars', 'bal', 'balance']
+handler.command = ['coins', 'wallet', 'cartera', 'Cebollines', 'Cebollin', 'bal', 'balance']
 handler.register = true 
 export default handler
 
