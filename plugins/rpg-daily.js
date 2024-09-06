fb16a1f90fd6fb8221c6dc1a428ba01bf69e60d9
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-05 22:09:28 -0500

    Update rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -7,12 +7,12 @@ let handler = async (m, { conn, isPrems }) => {
   const tiempoEspera = 24 * 60 * 60
   if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
     const tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-    conn.reply(m.chat, `🚩 Ya has realizado tu pedido gratis de hoy.\nRecuerda que solo puedes realizarlo 1 vez cada 24 horas.\n\n*Próximo Monto* : +${isPrems ? prem : free} 💫 XP\n*En* : ⏱ ${tiempoRestante}`, m, rcanal)
+    conn.reply(m.chat, `💙 Ya has realizado tu pedido gratis de hoy.\nRecuerda que solo puedes realizarlo 1 vez cada 24 horas.\n\n*Próximo Monto* : +${isPrems ? prem : free} 💫 XP\n*En* : ⏱ ${tiempoRestante}`, m, rcanal)
     return
   }
 
   global.db.data.users[m.sender].exp += isPrems ? prem : free
-  conn.reply(m.chat, `🚩 Felicidades 🎉, reclamaste *+${isPrems ? prem : free} 💫 XP*.`, m, rcanal)
+  conn.reply(m.chat, `💙 Felicidades 🎉, reclamaste *+${isPrems ? prem : free} 💫 XP*.`, m, rcanal)
 
   cooldowns[m.sender] = Date.now()
 }
