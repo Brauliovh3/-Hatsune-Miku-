c7c48d7a6d60cec493cd29c1930a566563712088
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-05 22:02:13 -0500

    Update serbot-bots.js

--- plugins/serbot-bots.js
+++ plugins/serbot-bots.js
@@ -8,7 +8,7 @@ global.conns.forEach((conn) => {
 if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
 uniqueUsers.set(conn.user.jid.replace(/[^0-9]/g, ''), conn.user)}})
 
-const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  ✩  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  ✩  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n└  ✩  *Nombre* : ${user.name || 'Ai Hoshino'}\n`
+const message = Array.from(uniqueUsers.values()).map((user, index) => `┌  💙  *${index + 1}* : @${user.jid.replace(/[^0-9]/g, '')}\n│  💙  *Link* : http://wa.me/${user.jid.replace(/[^0-9]/g, '')}\n└  ✩  *Nombre* : ${user.name || 'Ai Hoshino'}\n`
   ).join('\n')
   
 const replyMessage = message.length === 0 ? "" : message
