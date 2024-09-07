e6ae207d74395dce562979ad91d767306528e1f3
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-06 21:18:36 -0500

    Update youtube-ytsearch.js

--- plugins/youtube-ytsearch.js
+++ plugins/youtube-ytsearch.js
@@ -1,7 +1,7 @@
 import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-    if (!text) return conn.reply(m.chat, '🚩 Ingresa el título de un video o canción de YouTube.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m, rcanal)
+    if (!text) return conn.reply(m.chat, '💙 Ingresa el título de un video o canción de YouTube.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m, rcanal)
     await m.react('🕓')
     try {
     let results = await Starlights.ytsearch(text)
