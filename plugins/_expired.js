bd36d91a22e3f25a73a4197d3e635ed8ab9703c5
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-06 20:10:35 -0500

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -5,7 +5,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        await this.reply(m.chat, `🚩 Alquiler finalizado.`)
+        await this.reply(m.chat, `💙 Alquiler finalizado.`)
         await this.groupLeave(m.chat)
         chats.expired = null
     }
