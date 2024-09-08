e5b15c1cf7a1dbfc126044b347d9b9867058eac7
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-08 01:59:26 -0500

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -2,20 +2,19 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://api.github.com/repos/StarlightsTeam/Ai-Hoshino')
+let res = await fetch('https://github.com/Brauliovh3/-Hatsune-Miku')
 let json = await res.json()
 try {
 let txt = `*乂  S C R I P T  -  M A I N*\n\n`
-    txt += `	✩   *Nombre* : ${json.name}\n`
-    txt += `	✩   *Visitas* : ${json.watchers_count}\n`
-    txt += `	✩   *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
-    txt += `	✩   *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
-    txt += `	✩   *Url* : ${json.html_url}\n`
-    txt += `	✩   *Forks* : ${json.forks_count}\n`
-    txt += `	✩   *Stars* : ${json.stargazers_count}\n\n`
-    txt += `> 🚩 *${textbot}*`
-let img = await (await fetch(`https://i.ibb.co/LQKxczm/file.jpg`)).buffer()
-
+    txt += `	💙  *Nombre* : ${json.name}\n`
+    txt += `	💙   *Visitas* : ${json.watchers_count}\n`
+    txt += `	💙   *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
+    txt += `	💙   *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
+    txt += `	💙   *Url* : ${json.html_url}\n`
+    txt += `	💙   *Forks* : ${json.forks_count}\n`
+    txt += `	💙   *Stars* : ${json.stargazers_count}\n\n`
+    txt += `> 💙 *${textbot}*`
+let img = await (await fetch(`https://i.ibb.co/jGDYLKX/file.jpg`)).buffer()
 await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
 } catch {
 await m.react('✖️')
