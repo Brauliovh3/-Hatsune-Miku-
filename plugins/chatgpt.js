902a1447f41ea95aabd85d5b28e7723ee74809dd
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-08 14:37:59 -0500

    Update chatgpt.js

--- plugins/chatgpt.js
+++ plugins/chatgpt.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { text }) => {
    if (!text) return m.reply('Ingrese un Texto')
-   let res = await fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
+   let res = await fetch(`https://apis-starlights-team.koyeb.app/starlight/chatgpt?text=${text}&apikey=tamvan`)
    let json = await res.json()
    await m.reply(json.data.data ? json.data.data.trim() : json.data.message)
 }
