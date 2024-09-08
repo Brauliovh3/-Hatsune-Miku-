79ff60f6de71ca5652c3999482b6d340c5b39b6c
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-08 01:14:42 -0500

    Update group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -6,8 +6,8 @@ var number = text.split`@`[1]
 }else if(!isNaN(text)) {
 var number = text
 }
-if(!text && !m.quoted) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal)
+if(!text && !m.quoted) return conn.reply(m.chat, `💙 A quien putas Ascendo?.`, m, rcanal)
+if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `💙 A quien putas Ascendo?.`, m, rcanal)
 try {
 if(text) {
 var user = number + '@s.whatsapp.net'
@@ -19,7 +19,7 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-await conn.reply(m.chat, `🚩 Usuario degradado.`, m, rcanal)
+await conn.reply(m.chat, `💙 Usuario Descendido.`, m, rcanal)
 await m.react('✅')
 }
 
