64e165a3d4be28b2c3ab85253ae859a5845db555
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-08 02:38:16 -0500

    Update tools-ai.js

--- plugins/tools-ai.js
+++ plugins/tools-ai.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) return conn.reply(m.chat, `*🚩 Ingrese su petición*\n*🪼 Ejemplo de uso:* ${usedPrefix + command} como hacer estrella de papel`, m, rcanal)
+    if (!text) return conn.reply(m.chat, `*💙 Ingrese su petición*\n*🪼 Ejemplo de uso:* ${usedPrefix + command} como hacer estrella de papel`, m, rcanal)
     await m.react('💬')
 
     try {
