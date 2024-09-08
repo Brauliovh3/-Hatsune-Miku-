149b1357236c684b0c79c79a2da7986ea2623adb
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-08 02:04:45 -0500

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://github.com/Brauliovh3/-Hatsune-Miku-.git')
+let res = await fetch('https://api.github.com/repos/Brauliovh3/-Hatsune-Miku-')
 let json = await res.json()
 try {
 let txt = `*乂  S C R I P T  -  M A I N*\n\n`
