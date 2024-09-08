a3d34c894f0c1832a340489f10e9fc017b5179a1
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-08 01:36:32 -0500

    Update cmd-set.js

--- plugins/cmd-set.js
+++ plugins/cmd-set.js
@@ -1,12 +1,12 @@
 let handler = async (m, { text, usedPrefix, command }) => {
 global.db.data.sticker = global.db.data.sticker || {}
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde a un mensaje.`, m, rcanal)
-if (!m.quoted.fileSha256) return conn.reply(m.chat, `🚩 Responde a un mensaje.`, m, rcanal)
-if (!text) return conn.reply(m.chat, `🚩 Ingresa el nombre del comamdo.`, m, rcanal)
+if (!m.quoted) return conn.reply(m.chat, `💙 Responde a un mensaje.`, m, rcanal)
+if (!m.quoted.fileSha256) return conn.reply(m.chat, `💙 Responde a un mensaje.`, m, rcanal)
+if (!text) return conn.reply(m.chat, `💙 Ingresa el nombre del comamdo.`, m, rcanal)
 try {
 let sticker = global.db.data.sticker
 let hash = m.quoted.fileSha256.toString('base64')
-if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, `🚩 No tienes permiso para cambiar este comando de Sticker.`, m, rcanal)
+if (sticker[hash] && sticker[hash].locked) return conn.reply(m.chat, `💙 No tienes permiso para cambiar este comando de Sticker.`, m, rcanal)
 sticker[hash] = {
 text,
 mentionedJid: m.mentionedJid,
@@ -14,7 +14,7 @@ creator: m.sender,
 at: + new Date,
 locked: false,
 }
-await conn.reply(m.chat, `🚩 Comando guardado con exito.`, m, rcanal)
+await conn.reply(m.chat, `💙 Comando guardado con exito.`, m, rcanal)
 await m.react('✅')
 } catch {
 await m.react('✖️')
