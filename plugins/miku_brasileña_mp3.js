e0129b3a68febfef83eb24a1e3f8496703571ac4
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-11 17:44:30 -0500

    Update miku_brasileña_mp3.js

--- plugins/miku_brasileña_mp3.js
+++ plugins/miku_brasileña_mp3.js
@@ -1,13 +1,27 @@
-let util = require('util')
-let path = require('path')
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-let handler = async (m, { conn }) => {
-let vn = './storage/audio/miku brasileña.mp3'
-conn.sendFile(m.chat, vn, 'Banate.mp3', null, m, true, {
-type: 'audioMessage', 
-ptt: true
-})
+// Obtén el __dirname para ES Modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const handler = async (m, { conn }) => {
+    // Ruta al archivo de audio
+    const audioFilePath = path.join(__dirname, 'storage/audio/miku brasileña.mp3')
+    console.log('Enviando archivo desde:', audioFilePath)
+
+    try {
+        // Enviar el archivo como una nota de voz
+        await conn.sendFile(m.chat, audioFilePath, 'Banate.mp3', 'Aquí tienes el audio', m, true, {
+            type: 'audioMessage',
+            ptt: true // Esto asegura que el archivo se envíe como una nota de voz (PTT)
+        })
+        console.log('Archivo enviado como nota de voz con éxito.')
+    } catch (error) {
+        console.error('Error al enviar el archivo:', error)
+    }
 }
+
 handler.customPrefix = /bañate|Bañate/
 handler.command = new RegExp
-module.exports = handler
+
+export default handler
