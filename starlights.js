2139b5286c8f1ebb512ddd22e2cdb6dba42b053d
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-11 15:25:09 -0500

    Update starlights.js

--- starlights.js
+++ starlights.js
@@ -109,7 +109,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? ['kuroda', 'Safari', '2.0.0'] : methodCodeQR ? ['Ai Hoshino', 'Safari', '2.0.0'] : ['Ubuntu', 'Chrome', '110.0.5585.95'],
+browser: opcion == '1' ? ['(ㅎㅊDEPOOLㅊㅎ)', 'Safari', '2.0.0'] : methodCodeQR ? ['Hatsune Miku', 'Safari', '2.0.0'] : ['Ubuntu', 'Chrome', '110.0.5585.95'],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
