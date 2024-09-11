5bcdfa7e6c2fb6411705374ee105bad476631f22
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-11 16:20:50 -0500

    Add files via upload

---
+++ plugins/miku_brasileña_mp3.js
@@ -0,0 +1,13 @@
+let util = require('util')
+let path = require('path')
+
+let handler = async (m, { conn }) => {
+let vn = './media/Banate.mp3'
+conn.sendFile(m.chat, vn, 'Banate.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true
+})
+}
+handler.customPrefix = /bañate|Bañate/
+handler.command = new RegExp
+module.exports = handler
