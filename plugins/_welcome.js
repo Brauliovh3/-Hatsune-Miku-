54a2fb173c342a47911c786c0c930272f930a917
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-12 13:59:57 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -8,17 +8,17 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let chat = global.db.data.chats[m.chat]
 
   if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `┌─💙 *${botname}* \n│「 Bienvenido 」\n└┬💙 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+    let bienvenida = `┌─💙 *${botname}* \n│「 Como ta muchacho 」\n└┬💙 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🌱  Bienvenido a\n   │🌱  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
     
 await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
   }
   
   if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `┌─💙 *${botname}* \n│「 ADIOS 👋 」\n└┬💙 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+    let bye = `┌─💙 *${botname}* \n│「 ADIOS 🏳️‍🌈 」\n└┬💙 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🏳️‍🌈 fuera Kbro🏳️‍🌈\n   │🏳️‍🌈 Jamás te quisimos aquí\n   └───────────────┈ ⳹`
 await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
   }
   
   if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `┌─💙 *${botname}* \n│「 ADIOS 👋 」\n└┬💙 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+    let kick = `┌─💙 *${botname}* \n│「 ADIOS 🏳️‍🌈 」\n└┬💙 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🏳️‍🌈 La princesa se fue🏳️‍🌈\n   │🏳️‍🌈 Jamás te quisimos aquí\n   └───────────────┈ ⳹`
 await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
 }}
