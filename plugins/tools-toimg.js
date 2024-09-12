528db1b71a0ec6d08bcdefeba5795327ad79da51
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-12 13:41:12 -0500

    Update tools-toimg.js

--- plugins/tools-toimg.js
+++ plugins/tools-toimg.js
@@ -1,6 +1,6 @@
 import { webp2png } from '../lib/webp2mp4.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
-const notStickerMessage = `🚩 Responde a un *Sticker.*`
+const notStickerMessage = `💙 Responde a un *Sticker.*`
 if (!m.quoted) return conn.reply(m.chat, notStickerMessage, m, rcanal)
 const q = m.quoted || m
 let mime = q.mediaType || ''
@@ -9,7 +9,7 @@ await m.react('🕓')
 try {
 let media = await q.download()
 let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-await conn.sendFile(m.chat, out, 'thumbnail.jpg', listo, m)
+await conn.sendFile(m.chat, out, 'thumbnail.png', listo, m)
 await m.react('✅')
 } catch {
 await m.react('✖️')
