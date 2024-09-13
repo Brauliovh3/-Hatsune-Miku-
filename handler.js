254b75df92d6db9ae7bc04b45a1a90510540bc7e
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-13 18:04:05 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -411,7 +411,7 @@ global.dfail = (type, m, conn, usedPrefix) => {
         private: `💙 Hola, este comando solo puede ser utilizado en mi Chat *Privado*.`,
         admin: `💙 Hola, este comando solo puede ser utilizado por los *Administradores* del Grupo.`,
         botAdmin: `💙 Hola, la bot debe ser *Administradora* para ejecutar este Comando.`,
-        unreg: `💙 Hola, para usar este comando debes estar *Registrado.*\n\nPara usar el bot debes registrarte primero\n\nUtiliza: *.reg nombre.edad*\n\n_Ejemplo: *.reg (ㅎㅊDEPOOLㅊㅎ)⁩.18*_\n\nNo pongas los * *`,
+        unreg: `💙 Hola, para usar este comando debes estar *Registrado.*\n\nPara usar el bot debes registrarte primero\n\nUtiliza: *.reg nombre.edad*\n\n_Ejemplo: *.reg (ㅎㅊDEPOOLㅊㅎ)⁩.18*_\n`,
         restrict: `💙 Hola, esta característica está *deshabilitada.*`  
     }[type]
     if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))
