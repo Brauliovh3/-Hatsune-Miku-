bf4235e3e5e1657b17ba2cddfa5b136cc702c60c
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-13 23:11:30 -0500

    DEPOOL estuvo aqui

--- plugins/xnxxsearch.js
+++ plugins/xnxxsearch.js
@@ -1,12 +1,12 @@
 import fetch from 'node-fetch';
+import cheerio from 'cheerio'; // Asegúrate de que cheerio esté importado
 
-const handler = async (m, {text, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender]
+const handler = async (m, { text, usedPrefix, command }) => {
+  const datas = global;
+  const idioma = datas.db.data.users[m.sender];
 
-
-if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${tradutor.texto1} ${usedPrefix}enable modohorny*`;   
-  if (!text) throw `${tradutor.texto2} ${usedPrefix + command} Con mi prima*`;
+  if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${usedPrefix}enable modohorny*`;   
+  if (!text) throw `${usedPrefix + command} Con mi prima*`;
   try {
     const vids_ = {
       from: m.sender,
@@ -18,8 +18,10 @@ if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${tradutor.texto1} ${u
     if (global.videoListXXX[0]?.from == m.sender) {
       global.videoListXXX.splice(0, global.videoListXXX.length);
     }
+
     const res = await xnxxsearch(text);
     const json = res.result;
+    let cap = ''; // Inicializa 'cap' como una cadena vacía
     let count = 1;
     for (const v of json) {
       const linkXXX = v.link;
@@ -34,6 +36,7 @@ if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${tradutor.texto1} ${u
     throw e.message;
   }
 };
+
 handler.help = ['xnxxsearch'].map((v) => v + ' <query>');
 handler.tags = ['downloader', 'premium'];
 handler.command = /^xnxxsearch|xnxxs$/i;
@@ -42,29 +45,32 @@ export default handler;
 async function xnxxsearch(query) {
   return new Promise((resolve, reject) => {
     const baseurl = 'https://www.xnxx.com';
-    fetch(`${baseurl}/search/${query}/${Math.floor(Math.random() * 3) + 1}`, {method: 'get'}).then((res) => res.text()).then((res) => {
-      const $ = cheerio.load(res, {xmlMode: false});
-      const title = [];
-      const url = [];
-      const desc = [];
-      const results = [];
-      $('div.mozaique').each(function(a, b) {
-        $(b).find('div.thumb').each(function(c, d) {
-          url.push(baseurl + $(d).find('a').attr('href').replace('/THUMBNUM/', '/'));
-        });
-      });
-      $('div.mozaique').each(function(a, b) {
-        $(b).find('div.thumb-under').each(function(c, d) {
-          desc.push($(d).find('p.metadata').text());
-          $(d).find('a').each(function(e, f) {
-            title.push($(f).attr('title'));
+    fetch(`${baseurl}/search/${query}/${Math.floor(Math.random() * 3) + 1}`, { method: 'get' })
+      .then((res) => res.text())
+      .then((res) => {
+        const $ = cheerio.load(res, { xmlMode: false });
+        const title = [];
+        const url = [];
+        const desc = [];
+        const results = [];
+        $('div.mozaique').each(function (a, b) {
+          $(b).find('div.thumb').each(function (c, d) {
+            url.push(baseurl + $(d).find('a').attr('href').replace('/THUMBNUM/', '/'));
           });
         });
-      });
-      for (let i = 0; i < title.length; i++) {
-        results.push({title: title[i], info: desc[i], link: url[i]});
-      }
-      resolve({code: 200, status: true, result: results});
-    }).catch((err) => reject({code: 503, status: false, result: err}));
+        $('div.mozaique').each(function (a, b) {
+          $(b).find('div.thumb-under').each(function (c, d) {
+            desc.push($(d).find('p.metadata').text());
+            $(d).find('a').each(function (e, f) {
+              title.push($(f).attr('title'));
+            });
+          });
+        });
+        for (let i = 0; i < title.length; i++) {
+          results.push({ title: title[i], info: desc[i], link: url[i] });
+        }
+        resolve({ code: 200, status: true, result: results });
+      })
+      .catch((err) => reject({ code: 503, status: false, result: err }));
   });
 }
