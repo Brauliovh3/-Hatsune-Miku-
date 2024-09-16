5d3f87ff434d04cae191435c94dc57fdc7378ebe
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-16 18:30:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('NeKosmic\nVIP\nBot', {
+CFonts.say('Hatsune\nMiku\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
