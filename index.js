82412ae2a2368230c602bacfeb369307be5d3c73
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-16 18:30:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,85 +1,60 @@
 console.log('01 Iniciando 01')
-
-import { join, dirname } from 'path'
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts';
-import { createInterface } from 'readline'
-import yargs from 'yargs'
-import express from 'express'
-import chalk from 'chalk'
-import path from 'path'
-import os from 'os'
-import { promises as fsPromises } from 'fs'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname)
-const { say } = cfonts
-const rl = createInterface(process.stdin, process.stdout)
-
-const app = express()
-const port = process.env.PORT || 8080;
-
-say('Hatsune\nMiku', {
-font: 'chrome',
-align: 'center',
-gradient: ['cyan', 'cyan']})
+let { spawn } = require('child_process')
+let path = require('path')
+let fs = require('fs')
+let package = require('./package.json')
+const CFonts  = require('cfonts')
+CFonts.say('NeKosmic\nVIP\nBot', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
+CFonts.say(`Subscribete a Hatsune Miku - Bot\n\nBot creado por (ㅎㅊDEPOOLㅊㅎ)`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
 
 var isRunning = false
-
-async function start(files) {
-  if (isRunning) return;
-  isRunning = true;
-  
-  for (const file of files) {
-    const currentFilePath = new URL(import.meta.url).pathname;
-    let args = [join(__dirname, file), ...process.argv.slice(2)];
-    say([process.argv[0], ...args].join(' '), {
-      font: 'console',
-      align: 'center',
-      gradient: ['red', 'cyan']
-    });
-    
-    setupMaster({
-      exec: args[0],
-      args: args.slice(1),
-    });
-    
-    let p = fork();
-    p.on('message', data => {
-      console.log('[RECEIVED]', data);
-      switch (data) {
-        case 'reset':
-          p.process.kill();
-          isRunning = false;
-          start(files);
-          break;
-        case 'uptime':
-          p.send(process.uptime());
-          break;
-      }
-    });
-    
-    p.on('exit', (_, code) => {
-      isRunning = false;
-      console.error('Ocurrió un error inesperado:', code)
-      start(files);
-
-      if (code === 0) return;
-      watchFile(args[0], () => {
-        unwatchFile(args[0]);
-        start(files);
-      });
-    });
-    
-    let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
-    if (!opts['test'])
-      if (!rl.listenerCount()) rl.on('line', line => {
-        p.emit('message', line.trim());
-      });
-  }
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
+function start(file) {
+  if (isRunning) return
+  isRunning = true
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+  CFonts.say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  })
+  let p = spawn(process.argv[0], args, {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+  })
+  p.on('message', data => {
+    console.log('[RECEIVED]', data)
+    switch (data) {
+      case 'reset':
+        p.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  p.on('exit', code => {
+    isRunning = false
+    console.error('Ocurrio un error inesperado:', code)
+    if (code === 0) return
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
+      start(file)
+    })
+  })
+  // console.log(p)
 }
 
-start(['(ㅎㅊDEPOOLㅊㅎ).js'])
+start('main.js')
