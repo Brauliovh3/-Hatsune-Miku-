368dffbd4b40cd3714d29d4db3bce4305950468b
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-16 19:28:46 -0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,5 @@
 function handler(m) {
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-this.sendContact(m.chat, '34623442554', 'NeKosmic - Creador - Oficial 1', m)
-this.sendContact(m.chat, '56949581430', 'NeKosmic - Creador - Oficial 2', m)}
+this.sendContact(m.chat, '51988514570', 'Hatsune-Miku - Creador - Oficial 1', m)
 handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 module.exports = handler
