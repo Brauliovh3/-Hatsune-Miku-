3566fa28225e1245be3402d362860ac8a1439206
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-17 08:27:33 -0500

    Add files via upload

---
+++ plugins/main-grupos.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch'
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let img = await (await fetch(`https://i.ibb.co/PF9h29X/file.jpg`)).buffer()
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let txt = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad :D*
+
+1- https://chat.whatsapp.com/DiiCQW3hdEH6vXUkC4kgFG
+*💙* ${group}
+
+*─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ*
+
+➠ Enlace anulado? entre aquí! 
+
+Canal :
+*💙* ${canal}
+
+> 🚩 ${textbot}`
+await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, rcanal)
+}
+handler.help = ['grupos']
+handler.tags = ['main']
+handler.command = /^(grupos)$/i
+export default handler
