5ac50c1b0aef3092e11dbe90ede79a2872df6bae
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-23 22:38:48 -0500

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -5,14 +5,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
     let user = global.db.data.users[who]
-    if (!who) return m.reply(`🚩 Etiqueta a un usuario.`)
+    if (!who) return m.reply(`💙 Etiqueta a un usuario.`)
     let users = global.db.data.users
     users[who].banned = true
-    conn.reply(m.chat, `🚩 @${who.split`@`[0]} ha sido baneado con exito, ya no podrá volver a usar mis comandos.`, m, { mentions: [who] })
+    conn.reply(m.chat, `💙 @${who.split`@`[0]} ha sido baneado con exito, ya no podrá volver a usar mis comandos.`, m, { mentions: [who] })
 }
-handler.help = ['mban *@user*']
+handler.help = ['banuser *@user*']
 handler.tags = ['owner']
-handler.command = /^mban$/i
+handler.command = /^banuser$/i
 handler.rowner = true
 
 export default handler
