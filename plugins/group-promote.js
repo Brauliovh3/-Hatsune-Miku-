0a71be5983dae9cb0b95c23fa677c67cbafb78fc
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-24 08:47:56 -0500

    Update group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -6,8 +6,8 @@ var number = text.split`@`[1]
 }else if(!isNaN(text)) {
 var number = text
 }
-if(!text && !m.quoted) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `🚩 Menciona a una persona.`, m, rcanal)
+if(!text && !m.quoted) return conn.reply(m.chat, `💙 Menciona a una persona.`, m, rcanal)
+if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `💙 Menciona a una persona.`, m, rcanal)
 try {
 if(text) {
 var user = number + '@s.whatsapp.net'
@@ -19,7 +19,7 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-await conn.reply(m.chat, `🚩 Usuario promovido.`, m, rcanal)
+await conn.reply(m.chat, `💙 Usuario promovido.`, m, rcanal)
 await m.react('✅')
 }}
 handler.help = ['promote *@user*']
