18061dc6f1f5a1f00fb7178b82cfe890b824b89a
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-24 08:02:59 -0500

    Update group-groupInfo.js

--- plugins/group-groupInfo.js
+++ plugins/group-groupInfo.js
@@ -1,7 +1,7 @@
 //import db from '../lib/database.js'
 
 let handler = async (m, { conn, participants, groupMetadata }) => {
-    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
+    const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './storage/avatar_contact.jpg'
     const { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink, delete: del } = global.db.data.chats[m.chat]
     const groupAdmins = participants.filter(p => p.admin)
     const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
