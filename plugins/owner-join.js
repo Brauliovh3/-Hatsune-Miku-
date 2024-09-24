d0c71c17deae329018ea4914506a292bf00443ed
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-23 22:41:50 -0500

    Update owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -3,17 +3,17 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 	
-	if (!text) return m.reply(`🚩 Ingresa el enlace del Grupo.`)
+	if (!text) return m.reply(`💙 Ingresa el enlace del Grupo.`)
     let [_, code, expired] = text.match(linkRegex) || []
-    if (!code) return m.reply('🚩 Enlace invalido.')
+    if (!code) return m.reply('💙 Enlace invalido.')
     let res = await conn.groupAcceptInvite(code)
     expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
-    m.reply(`🚩 Me uní correctamente al Grupo *${res}${expired ? `* Durante *${expired}* días.` : ''}`)
+    m.reply(`💙 Me uní correctamente al Grupo *${res}${expired ? `* Durante *${expired}* días.` : ''}`)
     let chats = global.db.data.chats[res]
     if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
-    let pp = 'https://telegra.ph/file/4fa3f65b6698517cd8dcf.mp4'
-   await conn.sendMessage(res, { video: { url: pp }, gifPlayback: true, caption: 'Ya llego el mejor Bot de todo WhatsApp.', mentions: [m.sender] }, { quoted: estilo })
+    let pp = './storage/img/miniurl.mp4'
+   await conn.sendMessage(res, { video: { url: pp }, gifPlayback: true, caption: 'Ya llego Hatsune Miku tratame con cuidado.', mentions: [m.sender] }, { quoted: estilo })
 }
 handler.help = ['join *<link> <días>*']
 handler.tags = ['owner']
