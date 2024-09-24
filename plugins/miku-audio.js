6101f1aaa693d8e5feecdc20dbece4057a33c250
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-24 10:25:36 -0500

    Update and rename mikuaudio.js to miku-audio.js

---
+++ plugins/miku-audio.js
@@ -0,0 +1,12 @@
+export const handler = async (m, { conn }) => {
+    let vn = './media/miku.mp3';
+    conn.sendFile(m.chat, vn, 'miku.mp3', null, m, true, {
+        type: 'audioMessage',
+        ptt: true
+    });
+};
+
+handler.customPrefix = /Musica|musica/; 
+handler.command = new RegExp(); // Esto puede ser opcional
+
+export default handler; // Asegúrate de exportar el handler
