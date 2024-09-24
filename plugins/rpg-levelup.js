3c2665ff3520f2521db46ba3dbeba448d300b236
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-24 08:01:32 -0500

    Update rpg-levelup.js

--- plugins/rpg-levelup.js
+++ plugins/rpg-levelup.js
@@ -7,21 +7,21 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender]
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier)
-        let txt = ` –  *L E V E L U P  -  U S E R*\n\n`
-            txt += `┌  ✩  *Nombre* : ${name}\n`
-            txt += `│  ✩  *Nivel* : ${user.level}\n`
-            txt += `└  ✩  *XP* : ${user.exp - min}/${xp}\n\n`
+        let txt = ` –  *🌱L E V E L U P  -  U S E R🌱*\n\n`
+            txt += `┌  💙  *Nombre* : ${name}\n`
+            txt += `│  💙  *Nivel* : ${user.level}\n`
+            txt += `└  💙  *XP* : ${user.exp - min}/${xp}\n\n`
             txt += `Te falta *${max - user.exp}* de *💫 XP* para subir de nivel`
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
     }
     let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
     if (before !== user.level) {
-       let txt = ` –  *L E V E L U P  -  U S E R*\n\n`
-           txt += `┌  ✩  *Nombre* : ${conn.getName(m.sender)}\n`
-           txt += `│  ✩  *Nivel Anterior* : ${before}\n`
-           txt += `└  ✩  *Nivel Actual* : ${user.level}\n\n`
-           txt += `🚩 Cuanto más interactúes con *Hatsune Miku*, mayor será tu Nivel`
+       let txt = ` –  *🌱L E V E L U P  -  U S E R🌱*\n\n`
+           txt += `┌  💙 *Nombre* : ${conn.getName(m.sender)}\n`
+           txt += `│  💙  *Nivel Anterior* : ${before}\n`
+           txt += `└  💙  *Nivel Actual* : ${user.level}\n\n`
+           txt += `💙 Cuanto más interactúes con *Hatsune Miku*, mayor será tu Nivel`
 
 await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, rcanal)
         }
