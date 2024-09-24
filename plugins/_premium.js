780ae8df94cc888b561c46d2e75ed38e4659c82a
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-23 22:11:21 -0500

    Update _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -4,7 +4,7 @@ let user = global.db.data.users[m.sender]
 if (m.chat.endsWith('broadcast')) return
 if (user.premiumTime != 0 && user.premium) {
 if (new Date() * 1 >= user.premiumTime) {
-await m.reply(`🚩 @${m.sender.split`@`[0]} se terminó tu tiempo como usuario *Premium.*`, false, { mentions: [m.sender] })
+await m.reply(`💙 @${m.sender.split`@`[0]} se terminó tu tiempo como usuario *Premium.*`, false, { mentions: [m.sender] })
 user.premiumTime = 0
 user.premium = false 
 }}}
