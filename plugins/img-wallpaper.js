8e5dd64bd610bc0a4b7202737d743fae60b3cf7e
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-24 08:23:00 -0500

    Update img-wallpaper.js

--- plugins/img-wallpaper.js
+++ plugins/img-wallpaper.js
@@ -1,7 +1,7 @@
 import { googleImage, pinterest } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `🚩 Ingresa un texto junto al comando.\n\n*Ejemplo:*\n*${usedPrefix + command}* Ai Hoshino`, m, rcanal)
+if (!text) return conn.reply(m.chat, `💙 Ingresa un texto junto al comando.\n\n*Ejemplo:*\n*${usedPrefix + command}* Hatsune Miku`, m, rcanal)
 await m.react('🕓')
 try {
 let res = await (await googleImage('wallpaper' + text)).getRandom()
