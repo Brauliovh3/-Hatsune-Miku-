387755581fe7d7f8206dd412c2614ed1b998a4d7
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-29 00:53:51 -0500

    Update miku-audio.js

--- plugins/miku-audio.js
+++ plugins/miku-audio.js
@@ -1,4 +1,5 @@
-export const handler = async (m, { conn }) => {
+import fs from 'fs';
+let handler = async (m, { conn }) => {
     let vn = './media/miku.mp3';
     conn.sendFile(m.chat, vn, 'miku.mp3', null, m, true, {
         type: 'audioMessage',
