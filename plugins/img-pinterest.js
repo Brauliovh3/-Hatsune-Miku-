b9fe2e5fe91ed617ed5cb4b2348b9c0b25dc57e2
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-28 23:24:24 -0500

    Update img-pinterest.js

--- plugins/img-pinterest.js
+++ plugins/img-pinterest.js
@@ -1,7 +1,7 @@
 import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply('🚩 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Ai Hoshino Icons`)
+if (!text) return m.reply('💙 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Hatsune Miku Icons`)
 await m.react('🕓')
 try {
 let { dl_url } = await Starlights.pinterest(text)
