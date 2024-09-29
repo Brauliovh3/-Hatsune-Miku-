1824c8460a08853656c5bdad4b85c23b299ed326
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-09-29 02:00:03 -0500

    Update fun-follar.js

--- plugins/fun-follar.js
+++ plugins/fun-follar.js
@@ -1,19 +1,16 @@
-
-let handler = async (m, { conn, command, text }) => {
-if (!text) return m.reply(`*Ingresa el @ o el nombre de la persona que quieras saber si te puedes ${command.replace('how', '')}*`)
-try {
-let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-m.reply(`🤤👅🥵 *𝐀𝐂𝐀𝐁𝐀𝐒 𝐃𝐄 𝐅𝐎𝐋𝐋𝐀𝐑𝐓𝐄𝐋@!*🥵👅🤤
-
-*𝙏𝙚 𝙖𝙘𝙖𝙗𝙖𝙨 𝙙𝙚 𝙛𝙤𝙡𝙡𝙖𝙧 𝙖 𝙡𝙖 𝙥𝙚𝙧𝙧𝙖 𝙙𝙚* *${text}* ⁩ *𝙖 𝟰 𝙥𝙖𝙩𝙖𝙨 𝙢𝙞𝙚𝙣𝙩𝙧𝙖𝙨 𝙩𝙚 𝙜𝙚𝙢𝙞𝙖 𝙘𝙤𝙢𝙤 𝙪𝙣𝙖 𝙢𝙖𝙡𝙙𝙞𝙩𝙖 𝙥𝙚𝙧𝙧𝙖 "𝐀𝐚𝐚𝐡.., 𝐀𝐚𝐚𝐡𝐡, 𝐬𝐢𝐠𝐮𝐞, 𝐧𝐨 𝐩𝐚𝐫𝐞𝐬, 𝐧𝐨 𝐩𝐚𝐫𝐞𝐬.." 𝙮 𝙡𝙖 𝙝𝙖𝙨 𝙙𝙚𝙟𝙖𝙙𝙤 𝙩𝙖𝙣 𝙧𝙚𝙫𝙚𝙣𝙩𝙖𝙙𝙖 𝙦𝙪𝙚 𝙣𝙤 𝙥𝙪𝙚𝙙𝙚 𝙨𝙤𝙨𝙩𝙚𝙣𝙚𝙧 𝙣𝙞 𝙨𝙪 𝙥𝙧𝙤𝙥𝙞𝙤 𝙘𝙪𝙚𝙧𝙥𝙤 𝙡𝙖 𝙢𝙖𝙡𝙙𝙞𝙩𝙖 𝙯𝙤𝙧𝙧𝙖!*
-
-*${text}* 
-🤤🥵 *¡𝐘𝐀 𝐓𝐄 𝐇𝐀𝐍 𝐅𝐎𝐋𝐋𝐀𝐃𝐎!* 🥵🤤`, null, { mentions: [user] })
-} catch (err) {
-}}
-handler.help = ['follar']
-handler.tags = ['fun']
-handler.command = /^(Follar|violar)/i
-
-handler.register = true
-export default handler
+let handler = async (m, { conn, usedPrefix, command}) => {
+    let pp = 'https://tinyurl.com/28b8j9m7'
+    let pp2 = 'https://tinyurl.com/263szwef'
+    let who
+    if (m.isGroup) who = m.mentionedJid[0]
+    else who = m.chat
+    if (!who) return conn.reply(m.chat, '💙 Menciona al usuario con *@user*', m, rcanal)
+    let name2 = conn.getName(who)
+    let name = conn.getName(m.sender)
+    
+    await conn.sendMessage(m.chat, { video: { url: [pp, pp2].getRandom() }, gifPlayback: true, caption: `*${name}*` + 'Esta follando🍆🥒 a' + ` *${name2}*` + ' 💦💦 *:・ﾟ✧' }, { quoted: m })
+    }
+    handler.help = ['follar *<@user>*']
+    handler.tags = ['game']
+    handler.command = ['cum', 'follar']
+    export default handler
