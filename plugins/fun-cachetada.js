3efaac2005f4c5371f8a574a00e67c3a128b86ad
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-10-03 23:50:49 -0500

    Update fun-cachetada.js

--- plugins/fun-cachetada.js
+++ plugins/fun-cachetada.js
@@ -32,7 +32,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 }
 
 handler.help = ['slap @tag'];
-handler.tags = ['fun'];
+handler.tags = ['game'];
 handler.command = ['slap','bofetada','cachetada','Cachetada'];
 handler.group = true;
 
