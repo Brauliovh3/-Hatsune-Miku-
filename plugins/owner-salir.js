03aaf84d672c245e0fa74fffbbe770fc961f11bf
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-10-04 23:26:01 -0500

    Update owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4'
-await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, el Bot se despide! (≧ω≦)ゞ*', mentions: [m.sender] }, { quoted: estilo })
+await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, la Bot se despide! (T W T)ゞ*', mentions: [m.sender] }, { quoted: estilo })
 await conn.groupLeave(id)}
 handler.help = ['salir']
 handler.tags = ['owner']
