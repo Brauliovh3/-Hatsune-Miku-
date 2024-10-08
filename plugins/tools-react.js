84ecd4df2a12aa7003cf38716c454b1cccaa258f
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-10-07 20:03:09 -0500

    Update tools-react.js

--- plugins/tools-react.js
+++ plugins/tools-react.js
@@ -1,7 +1,7 @@
 let { proto } = (await import('@whiskeysockets/baileys')).default
 
 let handler = async (m, { conn, args, usedPrefix, command, text }) => {
-  if (!args[0]) return conn.reply(m.chat, `🚩 Responde a un mensaje y coloca el emoji junto al comando.`, m, rcanal)
+  if (!args[0]) return conn.reply(m.chat, `💙 Responde a un mensaje y coloca el emoji junto al comando.`, m, rcanal)
   let q = m.quoted ? await m.getQuotedObj() : m
  conn.sendMessage(m.chat, { react: { text: args[0], key: q.key }}, { quoted: q })
 }
