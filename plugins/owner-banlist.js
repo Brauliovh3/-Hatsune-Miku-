64b7af74834527a762eb57bf0eeeeeb550d922e3
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-10-08 22:49:57 -0500

    Update owner-banlist.js

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -3,13 +3,13 @@ let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned
 let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
 let caption = `
 ╭─「 *Usuarios Baneados* 」
-║❥ *Total* : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
-║❥ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
+║💙 *Total* : ${users.length} ${users ? '\n' + users.map(([jid], i) => `
+║💙 ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
 ╰────
 
 ╭─「 *Chats Baneados* 」
-║❥ *Total* : ${chats.length} ${chats ? '\n' + chats.map(([jid], i) => `
-║❥ ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
+║💙 *Total* : ${chats.length} ${chats ? '\n' + chats.map(([jid], i) => `
+║💙 ${isOwner ? '@' + jid.split`@`[0] : jid}`.trim()).join('\n') : '├'}
 ╰────
 `.trim()
 m.reply(caption, null, {mentions: conn.parseMention(caption)})}
