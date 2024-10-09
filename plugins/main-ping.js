27ad4d72fb29624069b020a397f580d06871b50d
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-10-09 14:00:26 -0500

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.reply(`*» Velocidad* : ${latensi.toFixed(4)} _ms_`);
+          m.reply(`*💙 Velocidad 🚄* : ${latensi.toFixed(4)} _ms_`);
             });
 }
 handler.help = ['ping']
