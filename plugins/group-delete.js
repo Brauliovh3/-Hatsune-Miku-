b6b352c0f3d49cbb7063926e3cc30661a95e8cbd
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2024-10-09 13:55:51 -0500

    Update group-delete.js

--- plugins/group-delete.js
+++ plugins/group-delete.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 	
-if (!m.quoted) return conn.reply(m.chat, `🚩 Responde al mensaje que deseas eliminar.`, m, rcanal)
+if (!m.quoted) return conn.reply(m.chat, `💙 Responde al mensaje que deseas eliminar.`, m, rcanal)
 try {
 let delet = m.message.extendedTextMessage.contextInfo.participant
 let bang = m.message.extendedTextMessage.contextInfo.stanzaId
